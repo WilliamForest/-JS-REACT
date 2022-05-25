29a2503bdb09dfc902faeee647984e3033542d0a
WilliamForest <Michel.04.11.00@gmail.com>
2022-05-25 13:10:12 +0300

    done task from video9

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Helo!')
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '1');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних фильмов?', 'Фантастические твари'),
+      b = prompt('Какую оценку поставить данному фильму?', '8.5'),
+      c = prompt('Один из последних фильмов?', ''),
+      d = prompt('Какую оценку поставить данному фильму?', '');
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
